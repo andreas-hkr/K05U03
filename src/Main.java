e689d1a2a304d31c781ea6f321ed103e89326b39
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 17:28:38 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Räkna ut antalet dagar det tar att tjäna ihop 10 miljoner
+        // Dag 1: 1 öre
+        // Dag 2: 2 öre
+        // Dag 4: 4 öre
+        // ...
+
+        
+    }
+}
